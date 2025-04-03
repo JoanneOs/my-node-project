a91fd8c120653ee0cff49d2c264e4598b70881a7
JoanneOs <jowan.awsman@yahoo.com>
2025-04-03 13:57:46 -0500

    setting up second project

--- server.js
+++ server.js
@@ -9,4 +9,25 @@
 // node_modules/
 // .env
 // This prevents unnecessary files from being pushed to GitHub.
+// git add .
+// git commit -m "Initial commit"
+// git push -u origin main
+
+//in json file the js file was index.js, corrected to server.js
+//command S for saving
+
+//npm start
+
+
+
+
 //writing simple server:
+
+// Load the HTTP module (built-in in Node.js)
+const http=require('http');
+
+// Create an empty function for now
+function handleRequest(){
+      // This function will handle requests later
+
+}
