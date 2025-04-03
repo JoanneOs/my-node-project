9bcff9b4c17ebfc405ffb15895a308d03006c93b
JoanneOs <jowan.awsman@yahoo.com>
2025-04-03 13:50:59 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//created folder 
+//npm init -y
+//created server.js
+//created new repo
+//git init
+// Add a .gitignore File (Important)
+// Create a new file .gitignore and add:
+
+// node_modules/
+// .env
+// This prevents unnecessary files from being pushed to GitHub.
+//writing simple server:
